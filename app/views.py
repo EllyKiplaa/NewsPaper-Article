ac607396caa23b41aa80d00b9b5335e89c9bacab
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-04 15:47:31 +0300

    Add the route news and the news function dynamic

--- app/views.py
+++ app/views.py
@@ -2,10 +2,11 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
-def index():
+# Views
+@app.route('/news/<int:news_id>')
+def movie(news_id):
 
     '''
-    View root page function that returns the index page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    return render_template('index.html')
+    return render_template('news.html',id = news_id)
