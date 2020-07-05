59fd9dc4a70561f5532d1aa884bde1c49050751d
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-05 13:49:45 +0300

    Import the unnitest and the News from models and create the instances of news

---
+++ app/news_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import News 
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News("bbc-news",'BBC News','The news site that will provide you with realtime news and what is happenning around the globe','bbc.com',"all-news","London,UK","en")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == '__main__':
+    unittest.main()
