57bdde221fff9cc323a0f591c8345af67dff8f17
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-06 01:13:41 +0300

    Add new files and new codes

---
+++ app/main/views.py
@@ -0,0 +1,33 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_news,get_articles
+
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+ 
+ # Getting popular top headlines
+    technology_news= get_news('technology')
+    business_news = get_news("business")
+    entertainment_news = get_news('entertainment')
+    sports_news = get_news('sports')
+    title = 'Get all the breaking and latest news Highlights'
+    
+    return render_template('index.html', title = title, technology = technology_news,   business=  business_news, entertainment = entertainment_news, sports =sports_news )
+
+@main.route('/news/<int:id>')
+def new_articles(id):
+    
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    news = get_news(id)
+    title = f'{id} All news Highlights'
+   
+
+    return render_template('headlines.html',title = title, news = news_articles )
+
