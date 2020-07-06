43c1dfeba2790819e8176a7f7da157de264dbc07
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-06 22:00:32 +0300

    add the dockstring

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 import os
 
 class Config:
+    '''
+    Configuration parent class
+    '''
 
-    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
