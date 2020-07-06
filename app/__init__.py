e9b8be71c2c6caa4a165afc6a0d92653c04b54c2
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-06 19:47:37 +0300

    fix a bug

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
     # Will add the views and forms
-# Registering the blueprint
+  # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
@@ -25,5 +25,3 @@ def create_app(config_name):
     configure_request(app)
 
     return app
-
-   
