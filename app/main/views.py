58074be066008b44c93bc39e0e6bb3646083f46e
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-09 15:58:34 +0300

    add the news articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_news
+from app.requests import get_news
 from ..models import News
+from app.requests import get_articles
+from unicodedata import category
 
 
 @main.route('/')
@@ -13,23 +15,24 @@ def index():
 
  
  # Getting popular top headlines
-    news_articles= get_news('')
+    articles= get_articles(category)
     # business_news = get_news("business")
     # entertainment_news = get_news('entertainment')
     # sports_news = get_news('sports')
     title = 'Get all the breaking and latest news Highlights'
     
-    return render_template('index.html', title = title, news=news_articles)
+    return render_template('index.html', title = title, news=articles)
 
-@main.route('/news/<int:id>')
-def new_articles(id):
+@main.route('/news')
+def news_articles():
     
     '''
     View news page function that returns the news details page and its data
     '''
-    articles = get_news(id)
+    news = get_news(id)
+    
     title = f'{id} All news Highlights'
    
 
-    return render_template('headlines.html',title = title, news = articles )
+    return render_template('news.html',title = title, list = news  )
 
