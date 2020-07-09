333699f9e6a7378f084ba2d2f406523c55d90866
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-09 16:09:05 +0300

    Fix a bug

--- config.py
+++ config.py
@@ -5,10 +5,13 @@ class Config:
     Configuration parent class
     '''
 
-    SOURCES_API_BASE_URL ='http://newsapi.org/v2/top-headlines?{}sources=bbc-news&{}api_key='
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&api_key={}'
-    
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+    NEWS_BASE_URL = 'https://newsapi.org/v2/everything?q=language=en&from=2020-07-07&to=2020-07-07&apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    @staticmethod
+    def init_app(app):
+        pass
     
 class ProdConfig(Config):
     pass
